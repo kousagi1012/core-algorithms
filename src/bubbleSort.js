0a1ff0eb8e697d96662316eb89a7499dc6c69d75
Greg <kousagi1012@yahoo.com>
2017-05-16 14:55:59 -0700

    bubbleSort complete

--- src/bubbleSort.js
+++ src/bubbleSort.js
@@ -1,2 +1,14 @@
 export default function bubbleSort (array) {
+  let swap, temp
+  do {
+    swap = false
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > array[i + 1]) {
+        temp = array[i]
+        array[i] = array[i + 1]
+        array[i + 1] = temp
+        swap = true
+      }
+    }
+  } while (swap)
 }
